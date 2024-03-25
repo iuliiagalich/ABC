af41b19af298886a829eff4b6bf18f9836932b2c
Галич Юлия <iuliiagalich@gmail.com>
2024-03-25 21:36:30 +0300

    Update abc.py

--- abc.py
+++ abc.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 # Считываем данные
 df = pd.read_csv(
-    "abc2.csv",
+    "data.csv",
     sep=";",
     decimal=",",
 )
@@ -14,24 +14,24 @@ index = "name"
 cols = list(df.columns)
 cols.remove(index)
 for col in cols:
-    groupped_df[f"rel_{col}"] = groupped_df[col] / sum(groupped_df[col])
-    groupped_df = groupped_df.sort_values(f"rel_{col}", ascending=False)
-    groupped_df[f"cumsum_{col}"] = groupped_df[f"rel_{col}"].cumsum()
-    groupped_df[f"abc_{col}"] = np.where(
-        groupped_df[f"cumsum_{col}"] < 0.8,
+    df[f"rel_{col}"] = df[col] / sum(df[col])
+    df = df.sort_values(f"rel_{col}", ascending=False)
+    df[f"cumsum_{col}"] = df[f"rel_{col}"].cumsum()
+    df[f"abc_{col}"] = np.where(
+        df[f"cumsum_{col}"] < 0.8,
         "A",
         np.where(groupped_df[f"cumsum_{col}"] < 0.95, "B", "C"),
     )
 
 # Сохраняем результат
-groupped_df[cols + [f"abc_{col}" for col in cols]].to_excel("1.xlsx", index=False)
+df[cols + [f"abc_{col}" for col in cols]].to_excel("1.xlsx", index=False)
 
 
 # Формируем итоговые наборы групп
-groupped_df["final_group"] = (
-    groupped_df["abc_revenue"] + groupped_df["abc_amount"] + groupped_df["abc_profit"]
+df["final_group"] = (
+    df["abc_revenue"] + df["abc_amount"] + df["abc_profit"]
 )
-groups = groupped_df.groupby("final_group")["final_group"].agg({"count"}).reset_index()
+groups = df.groupby("final_group")["final_group"].agg({"count"}).reset_index()
 groups["perc"] = groups["count"] / sum(groups["count"])
 
 # Код для построения графика treemap
